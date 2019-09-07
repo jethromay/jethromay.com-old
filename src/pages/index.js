6cc17765603af51491ce87aa05d5775d2c036260
Jethro May <jethro@plusnarrative.com>
2019-09-07 17:18:53 +0200

    Added github button and minor adjustments
    Signed-off-by: Jethro May <jethro@plusnarrative.com>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Layout from "../layout/layout"
 import SEO from "../components/seo"
+import GitHubButton from 'react-github-btn'
 
 const IndexPage = () => (
   <Layout>
@@ -10,14 +11,18 @@ const IndexPage = () => (
               <h1 className="font-bold text-6xl xs:text-5xl sm:text-5xl">Hi, I'm Jethro.</h1>
               <p className="mb-4">I am a web developer & writer with a passion for Laravel.</p>
               <div className="social flex">
-                  <iframe id="twitter-widget" scrolling="no" frameBorder="0" allowTransparency="true"
-                          className="twitter-follow-button twitter-follow-button-rendered"
-                          title="Twitter Follow Button"
-                          src="https://platform.twitter.com/widgets/follow_button.0639d67d95b7680840758b6833f06d87.en.html#dnt=false&amp;id=twitter-widget-0&amp;lang=en&amp;screen_name=may_jethro&amp;show_count=true&amp;show_screen_name=false&amp;size=l&amp;time=1564308648856"
-                          data-screen-name="may_jethro">
-                  </iframe>
-                  <a className="github-button" href="https://github.com/jethromay" data-size="large"
-                     data-show-count="true" aria-label="Follow @jethromay on GitHub">Follow</a>
+                  <a href="https://twitter.com/TwitterDev?ref_src=twsrc%5Etfw"
+                     className="twitter-follow-button"
+                     data-show-count="false">Follow @may_jethro
+                  </a>
+                  <GitHubButton
+                      href="https://github.com/jethromay"
+                      data-size="large"
+                      data-show-count="true"
+                      aria-label="Follow @jethromay on GitHub"
+                  >
+                      Follow
+                  </GitHubButton>
               </div>
           </div>
       </div>
