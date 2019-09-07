b57e1a2a811d339ce02ae8dc3007b6c064746e38
Jethro May <jethro@plusnarrative.com>
2019-09-07 15:45:28 +0200

    Added tailwindcss variables and markup updates
    Signed-off-by: Jethro May <jethro@plusnarrative.com>

--- src/components/layout.js
+++ src/components/layout.js
@@ -26,19 +26,19 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+      <div>
+        <main className="container mx-auto xs:px-4 sm:px-4 md:px-4 lg:px-0 mt-24 lg:mt-20">{children}</main>
+        <footer className="flex-none pt-10 pb-10 pr-5 pl-5">
+            <div className="flex items-center justify-center">
+                <span className="text-gray-500">© {new Date().getFullYear()}</span>
+            </div>
+            <div className="flex items-center justify-center">
+                <a href="#" className="text-gray-500 mr-2 ml-2 hover:text-blue-400">Ko-Fi</a>
+                <a href="#" className="text-gray-500 mr-2 ml-2 hover:text-blue-400">Twitter</a>
+                <a href="#" className="text-gray-500 mr-2 ml-2 hover:text-blue-400">GitHub</a>
+                <a href="#" className="text-gray-500 mr-2 ml-2 hover:text-blue-400">RSS</a>
+                <a href="#" className="text-gray-500 mr-2 ml-2 hover:text-blue-400">Source</a>
+            </div>
         </footer>
       </div>
     </>
