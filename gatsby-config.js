ca30bf45ab65e1ae8d74bad0dedca94f6bb1d339
Jethro May <jethro@plusnarrative.com>
2019-09-08 11:03:51 +0200

    Updated plugin manifest
    Signed-off-by: Jethro May <jethro@plusnarrative.com>

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,8 +30,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Jethro May`,
+        short_name: `Jethro May`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
