952e37cdcdf6f7cb9b89baeb642e9cfdfc11f4bd
Jethro May <jethro@plusnarrative.com>
2019-09-14 11:31:09 +0200

    Added missing custom breakpoints
    Signed-off-by: Jethro May <jethro@plusnarrative.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {},
+    screens: {
+      'xs': {'max': '639px'},
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    }
   },
   variants: {},
   plugins: []
-};
+}
