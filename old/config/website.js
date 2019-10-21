9d12d6231655618dae07bcf1d6c5f8583d491918
Jethro May <jethro@plusnarrative.com>
2019-10-21 12:43:24 +0200

    Initial commit
    Signed-off-by: Jethro May <jethro@plusnarrative.com>

--- old/config/website.js
+++
@@ -1,9 +0,0 @@
-const website = {
-    siteTitle: 'Jethro May',
-    siteDescription: 'Web Developer & Writer',
-    siteUrl: 'https://jethromay.com',
-    siteLogo: '',
-    twitterAccount: 'may_jethro'
-}
-
-module.exports = website
