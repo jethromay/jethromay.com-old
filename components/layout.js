c8cc277e3eb1f35d9240ad79eef367edbe15a716
Jethro May <jethro@plusnarrative.com>
2019-10-28 16:15:08 +0200

    Added _app.js file and minor tweaks

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,6 @@
 import React from "react"
 import Header from "../components/header"
 import Footer from "../components/footer"
-import "../public/css/main.css"
 
 const Layout = props => (
     <div className="flex flex-col min-h-full">
