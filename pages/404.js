14ae0467b74cca3d699da93885e1e10d2334f3d8
Jethro May <jethro@plusnarrative.com>
2019-11-09 10:45:06 +0200

    Added twitter button and various updates

--- pages/404.js
+++ pages/404.js
@@ -1,5 +1,3 @@
-import React from "react"
-
 export default class NotFoundPage extends React.Component {
     render() {
         return (
--- pages/articles.js
+++ pages/articles.js
@@ -1,5 +1,3 @@
-import React from "react"
-
 export default class Articles extends React.Component {
     render() {
         return (
