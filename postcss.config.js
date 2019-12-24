edd9dbe8465a7a59d5b09406c5c4f6460c3b3dca
Jethro May <jethro@plusnarrative.com>
2019-12-24 10:38:38 +0200

    Added purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,12 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+    content: ['./pages/**/*.mdx', './pages/**/*.js', './components/**/*.mdx', './components/**/*.js'],
+    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+});
+
 module.exports = {
     plugins: [
         require('tailwindcss'),
-        require('autoprefixer')
-    ]
+        require('autoprefixer'),
+        process.env.NODE_ENV === 'production' && purgecss,
+    ],
 };
