36f439809487af9dd6e9b1309ca5786d9ae305e6
Jethro May <jethro@plusnarrative.com>
2019-12-27 08:43:01 +0200

    Added badge to latest post, minor tweaks

--- components/postList.js
+++ components/postList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import ReactMarkdown from "react-markdown";
+import moment from "moment";
 
 const PostList = (props) => {
 
@@ -13,18 +14,27 @@ const PostList = (props) => {
         return date.toDateString().slice(4);
     }
 
+    function latestPost(date) {
+        return moment(date) > moment().subtract(1, 'months');
+    }
+
     return (
         <>
             {props.allPosts.map((post) => (
                 <article key={post.slug}>
                     <header>
-                        <h1 className="text-2xl mb-0">
-                            <Link href={{ pathname: `/posts/${post.slug}` }}>
-                                <a>
-                                    {post.document.data.title}
-                                </a>
-                            </Link>
-                        </h1>
+                        <div className="flex items-center">
+                            <h1 className="text-2xl mb-0 mr-3">
+                                <Link href={{ pathname: `/posts/${post.slug}` }}>
+                                    <a>
+                                        {post.document.data.title}
+                                    </a>
+                                </Link>
+                            </h1>
+                            {latestPost(post.document.data.date) && (
+                                <span className="rounded-full bg-green-500 px-3 py-1 mt-1 uppercase text-xs text-white font-bold mr-3">New</span>
+                            )}
+                        </div>
                         <time className="text-gray-600">{reformatDate(post.document.data.date)}</time>
                     </header>
                     <ReactMarkdown source={truncateSummary(post.document.content)} />
