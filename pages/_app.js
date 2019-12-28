d400add89974b2582870c2082adec2f8504ae10a
Jethro May <jethro@plusnarrative.com>
2019-12-28 13:22:43 +0200

    Added google analytics test code

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,10 @@
-import React from 'react'
-import App, { Container } from 'next/app'
-import '../public/css/main.css';
+import React from "react";
+import App, { Container } from "next/app";
+import Router from 'next/router'
+import * as gtag from '../lib/gtag';
+import "../public/css/main.css";
+
+Router.events.on('routeChangeComplete', url => gtag.pageview(url));
 
 class MyApp extends App {
     render() {
