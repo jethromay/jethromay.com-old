3a1a3ee2f85d4a4ae5e9b478c2a60decf4ea05a7
Jethro May <jethro@plusnarrative.com>
2019-12-29 14:45:24 +0200

    Fixed seo tags

--- components/layouts/post.js
+++ components/layouts/post.js
@@ -26,7 +26,7 @@ export default function Post({ meta, children }) {
         <div className="flex flex-col min-h-full">
             <Meta
                 siteTitle={meta.title ? `${meta.title} - Jethro May` : `Jethro May - Web Developer & Writer`}
-                siteDescription={config.siteDescription} />
+                siteDescription={meta.summary} />
             <Header />
             <main className="flex-grow container mx-auto xs:px-4 sm:px-4 md:px-4 lg:px-0 md:mt-20 lg:mt-20">
                 <article className="md:mt-20 lg:mb-20">
