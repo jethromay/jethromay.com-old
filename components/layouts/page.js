786be5f0eb49a973b28557c81a32a8b930a00661
Jethro May <jethro@plusnarrative.com>
2020-01-04 07:26:38 +0200

    Revert back to meta component

--- components/layouts/page.js
+++ components/layouts/page.js
@@ -1,15 +1,15 @@
 import React from "react";
 import Header from "../header";
 import Footer from "../footer";
-import { NextSeo } from 'next-seo';
 import config from "../../site.config";
+import Meta from "../meta";
 
 export default function Page({meta, children}) {
     return (
         <div className="flex flex-col min-h-full">
-            <NextSeo
-                title={`${meta.title} - Jethro May`}
-                description={config.siteDescription}
+            <Meta
+                siteTitle={`${meta.title} - Jethro May`}
+                siteDescription={meta.summary}
             />
             <Header />
             <main className="flex-grow container mx-auto xs:px-4 sm:px-4 md:px-4 lg:px-0 md:mt-20 lg:mt-20">
