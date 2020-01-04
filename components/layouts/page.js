b79672cfc65e8469b22c312ed198bb05b4df9499
Jethro May <jethro@plusnarrative.com>
2020-01-04 09:48:52 +0200

    Adjustments to page seo tags

--- components/layouts/page.js
+++ components/layouts/page.js
@@ -2,19 +2,22 @@ import React from "react";
 import Header from "../header";
 import Footer from "../footer";
 import Seo from "../seo";
-import config from "../../site.config";
+import {MDXProvider} from "@mdx-js/tag";
+import MarkdownComponent from "../markdown-component";
+import Page from "../page";
 
-export default function Page({meta, children}) {
+export default function PageLayout({meta, children}) {
     return (
         <div className="flex flex-col min-h-full">
-            <Seo
-                siteTitle={`${meta.title} - Jethro May`}
-                siteDescription={meta.summary}
-            />
             <Header />
             <main className="flex-grow container mx-auto xs:px-4 sm:px-4 md:px-4 lg:px-0 md:mt-20 lg:mt-20">
                 <article className="md:mt-20 lg:mb-20">
-                    {children}
+                    <MDXProvider components={MarkdownComponent}>
+                        <Page title={`${meta.title} - Jethro May`}>
+                            <Seo {...meta} />
+                        </Page>
+                        {children}
+                    </MDXProvider>
                 </article>
             </main>
             <Footer />
